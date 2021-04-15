f9deff174ad9c4d23615a484484061fa495d7995
James <jamesgault1488@gmail.com>
2021-04-15 13:03:34 -0400

    added file to service worker

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,28 +1,47 @@
 
-const FILES_TO_CACHE = [
-    "/", 
-    "/index.js",
-    "/styles.css",
-    "/icons/icon-192x192.png",
-    "/icons/icon-512x512.png"
-];
+// const FILES_TO_CACHE = [
+//     "/",
+//     "/index.js",
+//     "/styles.css",
+//     "/icons/icon-192x192.png",
+//     "/icons/icon-512x512.png"
+// ];
 
 
 
 const CACHE_NAME = "static-cache-v2";
 const DATA_CACHE_NAME = "data-cache-v1";
 
+const FILES_TO_CACHE = [
+    '/',
+    '/index.html',
+    '/index.js',
+    '/manifest.webmanifest',
+    '/styles.css',
+    '/icons/icon-152x152.png',
+    '/icons/icon-192x192.png'
+    
+];
+
 // install
 self.addEventListener("install", function (evt) {
+    // pre cache image data
     evt.waitUntil(
-        caches.open(CACHE_NAME).then(cache => {
-            console.log("Your files were pre-cached successfully!");
-            return cache.addAll(FILES_TO_CACHE);
+        caches.open(DATA_CACHE_NAME).then((cache) => cache.add("/api/transaction"))
+    );
+
+    // pre cache all static assets
+    evt.waitUntil(
+        caches.open(CACHE_NAME).then((cache) => cache.addAll(FILES_TO_CACHE)).catch(err => {
+            console.log(err);
         })
     );
 
+    // tell the browser to activate this service worker immediately once it
+    // has finished installing
     self.skipWaiting();
 });
+
 
 self.addEventListener("activate", function (evt) {
     evt.waitUntil(
@@ -41,61 +60,37 @@ self.addEventListener("activate", function (evt) {
     self.clients.claim();
 });
 
-// fetch
-self.addEventListener("fetch", function (evt) {
-    // cache successful requests to the API
-    if (evt.request.url.includes("/api/")) {
-        evt.respondWith(
-            caches.open(DATA_CACHE_NAME).then(cache => {
-                return fetch(evt.request)
-                    .then(response => {
-                        // If the response was good, clone it and store it in the cache.
-                        if (response.status === 200) {
-                            cache.put(evt.request.url, response.clone());
-                        }
+self.addEventListener('fetch', function (evt) {
+    // code to handle requests goes here
 
-                        return response;
-                    })
-                    .catch(err => {
-                        // Network request failed, try to get it from the cache.
-                        return cache.match(evt.request);
-                    });
-            }).catch(err => console.log(err))
+    if (evt.request.url.includes('/api/')) {
+        console.log('[Service Worker] Fetch (data)', evt.request.url);
+
+        evt.respondWith(
+            caches.open(DATA_CACHE_NAME).then(cache =>{
+                return fetch(evt.request)
+                .then(response => {
+                    if (response.status === 200) {
+                        cache.put(evt.request.url, response.clone());
+                    }
+
+                    return response;
+                })
+                .catch(err => {
+                    return cache.match(evt.request);
+                });
+            })
         );
 
         return;
     }
-
-    // evt.respondWith(
-    //     fetch(evt.request).catch(function() {
-    //         return cache.match(evt.request).then(function(response) {
-    //             if(response) {
-
-    //                 return response;
-    //             } else if (evt.request.headers.get("accept").includes("text/html")) {
-    //                 return cache.match("/")
-    //             }
-    //         })
-    //     })
-    // )
-
-
     
     evt.respondWith(
-        caches.match(evt.request).then(function (response) {
-            if(response) {
-                console.log(response);
-                return response
-            } else if (!response) {
-                fetch(evt.request)
-                console.log(evt.request);
-                console.log(response);
-            } else {
-                fetch(evt.request)
-                console.log(evt.request)
-                console.log(response);
-            }
-        }).catch(err => console.log(err))
+        caches.open(CACHE_NAME).then(cache => {
+            return cache.match(evt.request).then(response => {
+                return response || fetch(evt.request);
+            });
+        })
     );
 });
 
