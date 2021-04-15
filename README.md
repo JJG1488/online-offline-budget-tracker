0891316a391786b181a3327888a4b6ec3839f806
James <jamesgault1488@gmail.com>
2021-04-15 00:23:04 -0400

    added readme content

--- README.md
+++ README.md
@@ -1,2 +1,60 @@
-# online-offline-budget-tracker
-Shows offline capabilities
+## online-offline-budget-tracker
+
+# Description
+
+This application was created to display offline capability.
+
+---
+# Preview
+
+![Preview](public/icons/offline-tracker.png)
+
+![Link] (https://offline-tracker.herokuapp.com)
+
+---
+
+## Table of Contents
+
+[Description of Application](#description)
+
+[How to Install](#installation)
+
+[How to Use](#usage)
+
+[License Coverage](#license)
+
+[How to Contribute](#contributing)
+
+[Contact Information](#questions)
+    
+---
+
+# Installation
+
+To install just fork over the repository and run npm install in your IDE of choice. Run a node server.js in the terminal.
+
+---
+
+ # Usage
+
+Once the application is up, put the name of the type of transaction that you are going to make and the amount of the transaction in the field next to it. Click the add or subtract button to add or deduct from your total budget amount.
+
+---
+# License
+
+[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)
+
+---
+# Contributing
+
+Contact via email or github
+
+---
+# Questions
+
+jamesgault1488@gmail.com
+
+JJG1488
+
+Contact via email or github
+
--- public/service-worker.js
+++ public/service-worker.js
@@ -1,39 +1,29 @@
 
-
-// '/favicon.ico',  // commented out - because of uncertainty... // this goes under index.html if going to use 
-
 const FILES_TO_CACHE = [
-    '/',
-    '/index.html',
-    '/manifest.webmanifest',
-    '/styles.css',
-    '/icons/icon-192x192.png',
-    '/icons/icon-512x512.png',
+    "/", 
+    "/index.js",
+    "/styles.css",
+    "/icons/icon-192x192.png",
+    "/icons/icon-512x512.png"
 ];
+
+
 
 const CACHE_NAME = "static-cache-v2";
 const DATA_CACHE_NAME = "data-cache-v1";
 
-
-
 // install
 self.addEventListener("install", function (evt) {
-    // pre cache image data
     evt.waitUntil(
-        caches.open(DATA_CACHE_NAME).then((cache) => cache.add("/api/transaction"))
+        caches.open(CACHE_NAME).then(cache => {
+            console.log("Your files were pre-cached successfully!");
+            return cache.addAll(FILES_TO_CACHE);
+        })
     );
 
-    // pre cache all static assets
-    evt.waitUntil(
-        caches.open(CACHE_NAME).then((cache) => cache.addAll(FILES_TO_CACHE))
-    );
-
-    // tell the browser to activate this service worker immediately once it
-    // has finished installing
     self.skipWaiting();
 });
 
-//activate
 self.addEventListener("activate", function (evt) {
     evt.waitUntil(
         caches.keys().then(keyList => {
@@ -52,38 +42,60 @@ self.addEventListener("activate", function (evt) {
 });
 
 // fetch
-self.addEventListener('fetch', function (evt) {
-    // code to handle requests goes here
-
-    if (evt.request.url.includes('/api/')) {
-        console.log('[Service Worker] Fetch (data)', evt.request.url);
-
+self.addEventListener("fetch", function (evt) {
+    // cache successful requests to the API
+    if (evt.request.url.includes("/api/")) {
         evt.respondWith(
-            caches.open(DATA_CACHE_NAME).then(cache =>{
+            caches.open(DATA_CACHE_NAME).then(cache => {
                 return fetch(evt.request)
-                .then(response => {
-                    if (response.status === 200) {
-                        cache.put(evt.request.url, response.clone());
-                    }
+                    .then(response => {
+                        // If the response was good, clone it and store it in the cache.
+                        if (response.status === 200) {
+                            cache.put(evt.request.url, response.clone());
+                        }
 
-                    return response;
-                })
-                .catch(err => {
-                    // if network request failed, try to get it from the cache
-                    return cache.match(evt.request);
-                });
-            }).catch(err => {console.log(err);})
+                        return response;
+                    })
+                    .catch(err => {
+                        // Network request failed, try to get it from the cache.
+                        return cache.match(evt.request);
+                    });
+            }).catch(err => console.log(err))
         );
 
         return;
     }
+
+    // evt.respondWith(
+    //     fetch(evt.request).catch(function() {
+    //         return cache.match(evt.request).then(function(response) {
+    //             if(response) {
+
+    //                 return response;
+    //             } else if (evt.request.headers.get("accept").includes("text/html")) {
+    //                 return cache.match("/")
+    //             }
+    //         })
+    //     })
+    // )
+
+
     
     evt.respondWith(
-        caches.open(CACHE_NAME).then(cache => {
-            return cache.match(evt.request).then(response => {
-                return response || fetch(evt.request);
-            });
-        })
+        caches.match(evt.request).then(function (response) {
+            if(response) {
+                console.log(response);
+                return response
+            } else if (!response) {
+                fetch(evt.request)
+                console.log(evt.request);
+                console.log(response);
+            } else {
+                fetch(evt.request)
+                console.log(evt.request)
+                console.log(response);
+            }
+        }).catch(err => console.log(err))
     );
 });
 
